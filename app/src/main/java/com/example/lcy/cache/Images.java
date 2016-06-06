2676d7bb7989e7535cb21a0f90ba3196bf734a66
luchunyang <lcyseek@gmail>
2016-06-06 08:50:39 +0800

    提交

---
+++ app/src/main/java/com/example/lcy/cache/Images.java
@@ -0,0 +1,36 @@
+package com.example.lcy.cache;
+
+/**
+ * Created by luchunyang on 16/6/5.
+ */
+public class Images {
+    public final static String[] imageThumbUrls = new String[]{
+            "http://image.cnwest.com/attachement/jpg/site1/20111010/001372d8a36f0ffcd94d0a.jpg",
+            "http://www.sznews.com/ent/images/attachement/jpg/site3/20120409/0019b90e167810ec9b882e.jpg",
+            "http://www.ty163.cn/dou/uploads/allimg/20160416/1527_4660.jpg",
+            "http://img.bbs.cnhubei.com/forum/201212/16/084410afufudcd1w5wf5a5.jpg",
+            "http://newsxml.cnool.net/newspic2011/2013/2013-6/2013-6-25/635077481775000000.jpg",
+            "http://images.qianlong.com/mmsource/images/2012/08/06/debby101651102.jpg",
+            "http://gb.cri.cn/mmsource/images/2009/10/28/bf6759a26e694731a35712f453ca792d.jpg",
+            "http://i9.hexunimg.cn/2013-05-17/154221775.jpg",
+            "http://www.hinews.cn/pic/0/14/33/46/14334647_898103.jpg",
+            "http://www.gxdxhs.com/images/oa2s44linfwwoltdn5wq/t014cea5f96f5498fd5.jpg",
+            "http://img.zynews.com/attachement/jpg/site2/20121023/0021974e90a811f0e65606.jpg",
+            "http://www.58game.cn/static/uploads/news/20130614/51bab81324cc4.jpg",
+            "http://img5.hfhouse.com/news/content/2012-1-13/20120113102039453.jpg",
+            "http://img1.cache.netease.com/catchpic/E/E6/E604B018B1FAD162C212BF2551608821.jpg",
+            "http://pic.yesky.com/uploadImages/2016/115/12/3G38WQ3DZ26T.jpg",
+            "http://imgsrc.baidu.com/baike/pic/item/0b55b319ebc4b745a2919828cdfc1e178a821582.jpg",
+            "http://img1.imgtn.bdimg.com/it/u=1490750796,45601225&fm=21&gp=0.jpg",
+            "http://www.tj20.com/file/ent/images/201003/20100324091204346.jpg",
+            "http://fj2.eastday.com/hdqxb/20090819_9/node356636/images/01587585.jpg",
+            "http://i8.qhimg.com/t01be55afca0ee1188e.jpg",
+            "http://image83.360doc.com/DownloadImg/2015/03/1322/51127098_3.jpg",
+            "http://s15.sinaimg.cn/bmiddle/620bb208t77102462140e&690",
+            "http://img2.mtime.com/mg/2010/3/96fd281e-29bf-4a7c-ba45-f9bd1cf0a836.jpg",
+            "http://photo.l99.com/bigger/32/MjAwODEyMDEyMTQ1MTFfMTE5Ljk4LjY0LjEwNF84MDc0NjQ=.jpg",
+            "http://image.fvideo.cn/uploadfile/2014/02/07/20140207131751736009.jpg",
+            "http://img.chinasexq.com/uploadfiles/news/2016-2/1811834411.jpg",
+            "http://images.ccoo.cn/bbs/20120314/20120314125907750.jpg"
+    };
+}
--- app/src/main/java/com/example/lcy/cache/MainActivity.java
+++ app/src/main/java/com/example/lcy/cache/MainActivity.java
@@ -1,13 +1,91 @@
 package com.example.lcy.cache;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GridView mGridView;
+    private String [] imageThumbUrls = Images.imageThumbUrls;
+    private ImageAdapter mImageAdapter;
+    private FileUtils fileUtils;
+    private ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fileUtils = new FileUtils(this);
+        mGridView = (GridView) findViewById(R.id.gridView);
+        mImageAdapter = new ImageAdapter(this, mGridView, imageThumbUrls);
+        mGridView.setAdapter(mImageAdapter);
+
+        iv = (ImageView) findViewById(R.id.iv);
+
+    }
+
+    @Override
+    protected void onResume() {
+        /*
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    URL url = new URL("http://img.chinasexq.com/uploadfiles/news/2016-2/1811834411.jpg");
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setConnectTimeout(20000);
+                    connection.setReadTimeout(20000);
+                    int ret = connection.getResponseCode();
+                    System.out.println("------>"+ret);
+
+                    final Bitmap bitmap = BitmapFactory.decodeStream(connection.getInputStream());
+
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            iv.setImageBitmap(bitmap);
+                        }
+                    });
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        */
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        mImageAdapter.cancelTask();
+        super.onDestroy();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add("删除手机中图片缓存");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 0:
+                fileUtils.deleteFile();
+                Toast.makeText(getApplication(), "清空缓存成功", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
